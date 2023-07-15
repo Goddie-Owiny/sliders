63355ef18a0ac99d3832b1893b920e18cfa06868
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2023-07-15 16:21:36 +0300

    modifying the slides

--- slider.js
+++ slider.js
@@ -1,9 +1,22 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
- 
+const images = doxument.querySelectorAll('.images');
+
 let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () =>{
+   slider.style.transform = `translateX(-${slideNumber * 650}px)`;
+      slideNumber++;
+};
+
+const prevSlide = () =>{
+   slider.style.transform = `translateX(0px)`;
+      slideNumber = 1;
+};
+
 
 right.addEventListener('click',()=>{
-   slider.style.transform = `translateX(-650px)`;
+   slideNumber < length ? nextSlide() : prevSlide();
 })
